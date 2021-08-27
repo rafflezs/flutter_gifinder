13ff773d797164eb4ab4042339b76c1812d8a8a9
rafflezs <thi-rafael@hotmail.com.br>
2021-08-27 00:11:15 -0300

    Gif search done

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -21,7 +21,7 @@ class _HomePageState extends State<HomePage> {
           "https://api.giphy.com/v1/gifs/trending?api_key=FWD6OsPwiKuC189snT3QLm8GtF12oAYh&limit=30&rating=r"));
     } else {
       response = await http.get(Uri.parse(
-          "https://api.giphy.com/v1/gifs/search?api_key=FWD6OsPwiKuC189snT3QLm8GtF12oAYh&q=${_search}&limit=30&offset=${_offset}&rating=r&lang=en"));
+          "https://api.giphy.com/v1/gifs/search?api_key=FWD6OsPwiKuC189snT3QLm8GtF12oAYh&q=${_search}&limit=19&offset=${_offset}&rating=r&lang=en"));
     }
 
     return json.decode(response.body);
@@ -51,10 +51,16 @@ class _HomePageState extends State<HomePage> {
       body: Column(
         children: <Widget>[
           Padding(
-            padding: EdgeInsets.all(10.0),
+            padding: const EdgeInsets.all(10.0),
             child: TextField(
+              onSubmitted: (text) {
+                setState(() {
+                  _search = text;
+                  _offset = 0;
+                });
+              },
               textAlign: TextAlign.center,
-              decoration: InputDecoration(
+              decoration: const InputDecoration(
                 labelText: "Pesquise aqui: ",
                 labelStyle: TextStyle(color: Colors.white),
                 enabledBorder: OutlineInputBorder(
@@ -62,7 +68,7 @@ class _HomePageState extends State<HomePage> {
                 border: OutlineInputBorder(
                     borderSide: BorderSide(color: Colors.white)),
               ),
-              style: TextStyle(color: Colors.white, fontSize: 19),
+              style: const TextStyle(color: Colors.white, fontSize: 19),
             ),
           ),
           Expanded(
@@ -75,13 +81,17 @@ class _HomePageState extends State<HomePage> {
                       width: 250,
                       height: 250,
                       alignment: Alignment.center,
-                      child: CircularProgressIndicator(
+                      child: const CircularProgressIndicator(
                         valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
                         strokeWidth: 6,
                       ),
                     );
                   default:
-                    return Container();
+                    if (snapshot.hasError) {
+                      return Container();
+                    } else {
+                      return _gifGridBuilder(context, snapshot);
+                    }
                 }
               },
               future: _requestSearch(),
@@ -90,5 +100,59 @@ class _HomePageState extends State<HomePage> {
         ],
       ),
     );
+  }
+
+  int _getItemCount(List data) {
+    if (_search == null) {
+      return data.length;
+    } else {
+      return data.length + 1;
+    }
+  }
+
+  Widget _gifGridBuilder(BuildContext context, AsyncSnapshot snapshot) {
+    return GridView.builder(
+        padding: const EdgeInsets.all(10),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+        ),
+        itemCount: _getItemCount(snapshot.data["data"]),
+        itemBuilder: (context, index) {
+          if (_search == null || index < snapshot.data["data"].length) {
+            return GestureDetector(
+              child: Image.network(
+                snapshot.data["data"][index]["images"]["fixed_height"]["url"],
+                height: 300,
+                fit: BoxFit.cover,
+              ),
+            );
+          } else {
+            return Container(
+              child: GestureDetector(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const <Widget>[
+                    Icon(
+                      Icons.add,
+                      color: Colors.white,
+                      size: 80,
+                    ),
+                    Text("Carregar mais",
+                        style: TextStyle(
+                          color: Colors.white,
+                        ))
+                  ],
+                ),
+                onTap: () {
+                  setState(() {
+                    _offset += 19;
+                  });
+                },
+              ),
+            );
+          }
+        });
   }
 }
